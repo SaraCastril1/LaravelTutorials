510d7cc9bb59ebf531e8832442617ca56901dcb3
Sara Castrillón <84990901+SaraCastril1@users.noreply.github.com>
2024-02-15 11:41:44 -0500

    Add files via upload

---
+++ storage/framework/views/bc809532558ff4c7f7d7532f4104f9d1.php
@@ -0,0 +1,9 @@
+
+<?php $__env->startSection('title', 'Home Page - Online Store'); ?>
+<?php $__env->startSection('content'); ?>
+<div class="text-center">
+  Welcome to the application
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\2024-1\TEIS\talleres\tutorial1\resources\views/home/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f918666991cb2a34a488db350238bbf3.php
@@ -0,0 +1,25 @@
+
+<?php $__env->startSection('title', $viewData["title"]); ?>
+<?php $__env->startSection('subtitle', $viewData["subtitle"]); ?>
+<?php $__env->startSection('content'); ?>
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://laravel.com/img/logotype.min.svg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">
+           <?php echo e($viewData["product"]["name"]); ?>
+
+        </h5>
+        <p class="card-text"><?php echo e($viewData["product"]["description"]); ?></p>
+        <span>$</span>
+        <span class="card-text"><?php echo e($viewData["product"]["price"]); ?></span> 
+      </div>
+    </div>
+  </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\2024-1\TEIS\talleres\tutorial1\resources\views/product/show.blade.php ENDPATH**/ ?>
