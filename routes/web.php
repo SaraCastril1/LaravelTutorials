1985210d5e5b1e48396988f957031a879acbc9bc
SaraCastril1 <smcastril1@eafit.edu.co>
2024-03-04 14:43:09 -0500

    feat:tutorial3

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,13 @@ Route::get('products/validation', 'App\Http\Controllers\ProductController@valida
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
 
 Route::get('/contact/', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
+
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
+
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
---
+++ storage/framework/views/d52bdf20cbaa84fd7e943659b8a5e25f.php
@@ -0,0 +1,28 @@
+
+<?php $__env->startSection("title", "Image Storage - DI"); ?>
+<?php $__env->startSection('content'); ?>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Upload image</div>
+        <div class="card-body">
+
+        <form action="<?php echo e(route('imagenotdi.save')); ?>" method="post" enctype="multipart/form-data">
+          <?php echo csrf_field(); ?>
+          <div class="form-group">
+            <label>Image:</label>
+            <input type="file" name="profile_image" />
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+        <img src="<?php echo e(URL::asset('storage/test.png')); ?>" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\2024-1\TEIS\laravelTutorials\LaravelTutorials\resources\views/imagenotdi/index.blade.php ENDPATH**/ ?>
